88ec1dec6ef628be73fde74806a1532d4d68856b
Egide Muhire <saladthieves@gmail.com>
2020-10-23 19:08:41 +0200

    End of class 11

--- app/src/main/java/com/gebeya/fetan/HomeActivity.kt
+++ app/src/main/java/com/gebeya/fetan/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import com.gebeya.fetan.framework.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.runs_summary_layout.*
 
 class HomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +14,12 @@ class HomeActivity : BaseActivity() {
         homeAddRunButton.setOnClickListener {
             startActivity(Intent(this, AddRunActivity::class.java))
         }
+
+        // 1) pass in null => root element of the layout android:layout_centerInParent="true"
+        // 2) pass in viewGroup => viewGroup returned with inflated layout as child
+        val root = layoutInflater.inflate(R.layout.runs_summary_layout, summaryContainer)
+
+        runsSummaryTime.text = "0 mins"
+        runsSummaryDistance.text = "0 meters"
     }
 }
