036ad0d83227d56943ab6b676ca52f215b4ebdd2
Egide Muhire <saladthieves@gmail.com>
2020-10-24 15:55:46 +0200

    End of class 12

--- app/src/main/java/com/gebeya/fetan/AboutActivity.kt
+++ app/src/main/java/com/gebeya/fetan/AboutActivity.kt
@@ -1,11 +1,54 @@
 package com.gebeya.fetan
 
+import android.animation.Animator
+import android.animation.AnimatorInflater
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_about.*
+import java.util.*
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val animator = AnimatorInflater.loadAnimator(this, R.animator.shoe_animation)
+        animator.apply {
+            setTarget(aboutShoeIcon)
+        }
+
+        animator.addListener(object: Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                animation?.start()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+            }
+        })
+
+        animator.start()
+
+        aboutShoeIcon.setOnClickListener {
+            val amount = Random().nextInt(5) + 1
+            val value = Random().nextInt(2)
+            val direction = if (value == 0) -1 else 1
+            val rotate = ObjectAnimator.ofFloat(aboutShoeIcon, "rotation", 0f, amount * 360f * direction)
+            rotate.apply {
+                duration = 5000
+                start()
+            }
+            val rootRotate = ObjectAnimator.ofFloat(aboutRoot, "rotation", 0f, amount * 360f * -direction)
+            rootRotate.apply {
+                duration = 5000
+                start()
+            }
+        }
     }
 }
--- app/src/main/java/com/gebeya/fetan/HomeActivity.kt
+++ app/src/main/java/com/gebeya/fetan/HomeActivity.kt
@@ -1,5 +1,8 @@
 package com.gebeya.fetan
 
+import android.animation.Animator
+import android.animation.AnimatorInflater
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import com.gebeya.fetan.framework.base.BaseActivity
@@ -12,6 +15,29 @@ class HomeActivity : BaseActivity() {
 
         homeAddRunButton.setOnClickListener {
             startActivity(Intent(this, AboutActivity::class.java))
+        }
+
+//        with(AnimatorInflater.loadAnimator(this, R.animator.add_animation)) {
+//            setTarget(homeAddRunButton)
+//            start()
+//        }
+
+        val animator = ObjectAnimator.ofFloat(homeNoRunsLabel, "alpha", 0f, 1f)
+        animator.apply {
+            duration = 800
+            startDelay = 400
+            start()
+        }
+
+        val backgroundAnimator = ObjectAnimator.ofInt(
+            homeAddRunButton,                // target
+            "marginTop", // property
+            0
+        )
+        backgroundAnimator.apply {
+            duration = 800
+            startDelay = 400
+            start()
         }
     }
 }
